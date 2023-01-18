d5dbcc94a72358d90937ff6d8acc9dc484f93d03
Omar Nassar <omariio1000@gmail.com>
2023-01-18 11:28:36 -0800

    update lab0

--- Lab0/L0_p3.py
+++ Lab0/L0_p3.py
@@ -5,7 +5,8 @@ Sx = float(input("Enter the scaling factor Sx: "))
 Sy = float(input("Enter the scaling factor Sy: "))
 ThDeg = float(input("Enter the rotation angle (deg): "))
 
-Th = np.radians(-ThDeg)
+Th = np.radians(ThDeg)
+#Th2 = np.radians(-ThDeg)
 
 og = np.array([(0,0), (0,1), (1,0), (0,0)])
 #print(og, '\n')
@@ -14,10 +15,12 @@ scale = np.array([(Sx, 0), (0, Sy)])
 rot = np.array([(np.cos(Th), -np.sin(Th)), 
                 (np.sin(Th), np.cos(Th))])
 
-transform = np.matmul(scale, rot)
+transform = np.matmul(rot, scale)
+#transform = np.matmul(scale, rot)
 print(transform, '\n')
 
-new = np.matmul(og, transform)
+new = np.matmul(transform, og.transpose()).transpose()
+#new = np.matmul(og, transform)
 #print(new, '\n')
 
 print("\nAf = [%.4f, %.4f]" % (new[0][0], new[0][1]))
